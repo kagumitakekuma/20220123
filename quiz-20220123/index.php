7cd5753c9302e3066bc55490254685accf3e41d9
Kagumi Takekuma <80142118+kagumitakekuma@users.noreply.github.com>
2022-01-23 17:15:05 +0900

    Add files via upload

---
+++ quiz-20220123/index.php
@@ -0,0 +1,40 @@
+<?php
+
+ini_set('display_errors',1);
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Quiz</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+ <div id="container"></div>
+<h1>Q. What is A?</h1>
+<ul>
+    <li class="answer">A0</li>
+    <li class="answer">A1</li>
+    <li class="answer">A2</li>
+    <li class="answer">A3</li>
+
+</ul>
+<div id="btn" class="disabled">Next Question</div>
+
+  </body>
+</html>
+<?php
+
+
+
+
+
+?>
